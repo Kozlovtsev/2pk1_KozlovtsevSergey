a28032808dbebc651b4e1dd9c6d33642c58a9ca5
сергей козловцев <blackatom21@mail.ru>
2022-09-22 20:10:45 +0500

    10 вариант

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace pz_5
+﻿using System;
+
+namespace pz_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 30; i <= 100; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Первое задание выполнено");
+
+
+            for (char a = 'L'; a <= 'R'; a++)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Второе задание выполнено");
+
+            int n = 4;
+            int m = 4;
+            for (int i = 0; i < m; i++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Третье задание выполнено");
+
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 9 == 0)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+            Console.WriteLine("Четвертое задание выполнено");
+
+
+            for (int i = 4, j = 50; i - j <= 11; i++, j--)
+            {
+                Console.WriteLine(i - j);
+            }
+            Console.WriteLine("Пятое задание выполнено");
         }
+
     }
 }
