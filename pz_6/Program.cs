88ab6f32ef5e64923ab954a688f2b6582287bd1d
Kozlovtsev <114168090+Kozlovtsev@users.noreply.github.com>
2022-09-26 13:53:39 +0500

    Add files via upload

---
+++ pz_6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_6
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите длинну отрезка А:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите длинну отрезка В:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int box = 0;
+                int c = a - b;
+                while (c >= 0)
+                {
+                    box++;
+                    c = c - b;
+                }
+                Console.WriteLine($"Количество отрезков В в отрезке А: {box}");
+            }
+        }
+}
