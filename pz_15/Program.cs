71212b0520bd79b872afae8cf6e49c7a335aae95
Kozlovtsev <114168090+Kozlovtsev@users.noreply.github.com>
2022-11-26 05:37:57 +0500

    Add files via upload

---
+++ pz_15/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] txt1 = File.ReadAllLines(@"B:\прогги\zadanie.txt");
+            string[] txt2 = new string[txt1.Length];
+            for (int i = 0; i < txt1.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    txt2[i] = txt1[i];
+                }
+            }
+            for (int i = 0; i < txt2.Length;i++)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(@"B:\прогги\zadanie.txt", true))
+                {
+                    streamWriter.WriteLine(txt2[i] + "\n");
+                }
+            }
+        }
+    }
+}
